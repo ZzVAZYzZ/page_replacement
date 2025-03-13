9e8de0c635753f3a3a20f3fe83e201cb44b8b852
Vazy <86739701+ZzVAZYzZ@users.noreply.github.com>
2025-03-14 01:11:44 +0700

    update tutor

--- utils/tutor.js
+++ utils/tutor.js
@@ -6,10 +6,11 @@ export const tutor = Object.freeze({
             </div>
             <div>
                 <ul>
-                    <li><p>Step 1: Choose Page Replacement algorithm.</p></li>
-                    <li><p>Step 2: Choose FRAME (1, 2, 3, 4, or 5).</p></li>
-                    <li><p>Step 3: Enter the reference string into the input box.</p></li>
-                    <li><p>Step 4: Click the Run button.</p></li>
+                    <li><p>Step 1: Select a Page Replacement algorithm.</p></li>
+                    <li><p>Step 2: Choose the number of frames (1, 2, 3, 4, 5 or custom).</p></li>
+                    <li><p>Step 3: Enter the reference string in the input box.</p></li>
+                    <li><p>Step 4: Click the "Run" button to execute.</p></li>
+                    <li><p>(<strong>Note:</strong> The Optimal algorithm in this tool may differ. Please check other references for a better understanding.)</p></li>
                 </ul>
             </div>
         </div>
